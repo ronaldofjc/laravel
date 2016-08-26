f6dc23998e520d83d9f614dc05c6c3d215554721
Ronaldo <ronaldofjc@hotmail.com>
2016-08-25 23:15:40 -0300

    Atualizaçao

--- estoque/app/Http/routes.php
+++ estoque/app/Http/routes.php
@@ -17,6 +17,6 @@ Route::get('/', function () {
 
 Route::get('/produtos', 'ProdutoController@lista');
 
-Route::get('/produtos/mostra/{id}', 'ProdutoController@mostra');
+Route::get('/produtos/mostra/{id}', 'ProdutoController@mostra')->where('id', '[0-9]+');
 
 
